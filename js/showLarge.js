01680902398293243fd531f865c071e0393d98fc
54Cheryl <cherylhsu246@gmail.com>
2022-04-25 19:07:20 +0800

    新增產品

--- js/showLarge.js
+++ js/showLarge.js
@@ -16,7 +16,7 @@ $(function(){
 });
 $(function(){
     $('.one_model').click(function(){
-        let a = '小孩的完美保姆';
+        let a = '完美保姆';
         let b = 'AZ110';
         let c = '照護';
         let d = '料理';
@@ -29,10 +29,10 @@ $(function(){
 });
 $(function(){
     $('.two_model').click(function(){
-        let a = '符合手作的需要';
-        let b = 'NC200';
-        let c = '設計';
-        let d = '手工';
+        let a = '家庭教師';
+        let b = 'CP100';
+        let c = '教育';
+        let d = '陪伴';
         // alert(ab);
         $('.product_maintag').text(a);
         $('.productModel').text(b);
@@ -42,7 +42,7 @@ $(function(){
 });
 $(function(){
     $('.three_model').click(function(){
-        let a = '家中的護理師';
+        let a = '家中護理師';
         let b = 'SD600';
         let c = '照護';
         let d = '醫療';
@@ -55,10 +55,62 @@ $(function(){
 });
 $(function(){
     $('.four_model').click(function(){
-        let a = '私人主廚!';
+        let a = '私人主廚';
         let b = 'SK700';
         let c = '料理';
         let d = '營養';
+        // alert(ab);
+        $('.product_maintag').text(a);
+        $('.productModel').text(b);
+        $('.tag_first span').text(c);
+        $('.tag_last span').text(d);
+    });
+});
+$(function(){
+    $('.five_model').click(function(){
+        let a = '完美助理';
+        let b = 'HW00';
+        let c = '料理';
+        let d = '營養';
+        // alert(ab);
+        $('.product_maintag').text(a);
+        $('.productModel').text(b);
+        $('.tag_first span').text(c);
+        $('.tag_last span').text(d);
+    });
+});
+$(function(){
+    $('.six_model').click(function(){
+        let a = '終身摯友';
+        let b = 'GJ200';
+        let c = '陪伴';
+        let d = '感情';
+        // alert(ab);
+        $('.product_maintag').text(a);
+        $('.productModel').text(b);
+        $('.tag_first span').text(c);
+        $('.tag_last span').text(d);
+    });
+});
+$(function(){
+    $('.seven_model').click(function(){
+        let a = '符合手作需要';
+        let b = 'NC200';
+        let c = '設計';
+        let d = '手工';
+        // alert(ab);
+        $('.product_maintag').text(a);
+        $('.productModel').text(b);
+        $('.tag_first span').text(c);
+        $('.tag_last span').text(d);
+    });
+});
+$(function(){
+    $('.eight_model').click(function(){
+        let a = '完美服務員';
+        let b = 'OA400';
+        let c = '清潔';
+        let d = '維護';
         // alert(ab);
         $('.product_maintag').text(a);
         $('.productModel').text(b);
